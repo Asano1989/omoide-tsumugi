abd8d5f38455fe5490d9b182ae389c5ee979bddc
Asano1989 <asano1989.b@gmail.com>
2026-01-07 23:50:55 +0900

    update: ログアウト機能を修正

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -77,29 +77,14 @@ class AuthController < ApplicationController
   end
 
   def destroy
-    is_secure = request.protocol == 'https://'
+    # Railsのセッションを空にする
+    reset_session
     
-    # 本番環境でのみ secure: true にする場合
-    # is_secure = Rails.env.production?
-
-    # 1. Rails側で使用しているCookieキーを削除
-    cookie_options = {
-      path: '/',
-      same_site: :lax,
-      secure: is_secure
-    }
-
-    cookies.delete(:rails_access_token, cookie_options)
+    # クッキーの削除
+    cookies.delete(:rails_access_token, path: '/')
     
-    # 2. メモ化された @current_user を明示的にリセットする
-    #    次のリクエストで current_user が呼ばれた際に、認証ロジックを強制的に再実行させるため
     @current_user = nil
-
-    # 3. フラッシュメッセージを設定
-    flash[:notice] = "ログアウトしました。"
-    
-    # 4. ページ全体を強制的にリロードさせるリダイレクト
-    redirect_to auth_path(force_signout: true), status: :see_other
+    redirect_to root_path, notice: "ログアウトしました。", status: :see_other
   end
 
   def set_cookie
