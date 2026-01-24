4bfa2ca09db9dd3ed1d52b649bc4f6bf1a67b1f7
Asano1989 <asano1989.b@gmail.com>
2026-01-24 12:29:33 +0900

    update: Lintチェック対応：余分な空白

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   def flash_background_color(type)
     case type.to_sym
     when :notice then "bg-green-100 border border-green-400  text-green-700"
-    when :alert  then "bg-red-100 border border-red-400 text-red-700"
-    when :error  then "bg-yellow-100 border border-yellow-400 text-yellow-700"
+    when :alert then "bg-red-100 border border-red-400 text-red-700"
+    when :error then "bg-yellow-100 border border-yellow-400 text-yellow-700"
     else "bg-gray-100 border border-gray-400 text-gray-700"
     end
   end
@@ -11,6 +11,6 @@ module ApplicationHelper
   def page_title(title)
     base_title = 'おもいでつむぎ'
 
-    title.blank? ? base_title : title + " | " +  base_title
+    title.blank? ? base_title : title + " | " + base_title
   end
 end
