39c7607e703cb65edb7cfd8eeb75dfb5a48af76f
Asano1989 <asano1989.b@gmail.com>
2026-01-24 16:19:24 +0900

    update: Lintチェック対応：メソッドの切り出し

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,20 +9,12 @@ module Api
 
       # POST /api/v1/users/register_on_rails
       def register_on_rails
-        # supabase_uid で既存ユーザーを探す、なければ新しく作る
-        @user = User.find_or_initialize_by(supabase_uid: user_params[:supabase_uid])
-
+        set_user_supabase_uid
         is_new_record = @user.new_record?
-
-        @user.email = user_params[:email] if user_params[:email].present?
-        @user.name = user_params[:name] if user_params[:name].present?
-        @user.birthday = user_params[:birthday] if user_params[:birthday].present?
-        @user.avatar.attach(user_params[:avatar]) if user_params[:avatar].present?
-
+        set_user
         if @user.save
           # 新規か既存かでメッセージを切り替える
           msg = is_new_record ? 'ユーザー登録が完了しました。' : 'ログインしました。'
-
           render json: { status: 'success', message: msg, user: @user }, status: :ok
         else
           render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
@@ -32,6 +24,18 @@ module Api
       rescue => e
         render json: { error: "Internal Server Error: #{e.message}" }, status: :internal_server_error
       end
+    end
+
+    def set_user_supabase_uid
+      # supabase_uid で既存ユーザーを探す、なければ新しく作る
+      @user = User.find_or_initialize_by(supabase_uid: user_params[:supabase_uid])
+    end
+
+    def set_user
+      @user.email = user_params[:email] if user_params[:email].present?
+      @user.name = user_params[:name] if user_params[:name].present?
+      @user.birthday = user_params[:birthday] if user_params[:birthday].present?
+      @user.avatar.attach(user_params[:avatar]) if user_params[:avatar].present?
     end
   end
 end
