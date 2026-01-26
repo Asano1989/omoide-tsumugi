b85ce98a8de7b17fd2d7cd13b21859c3b41b83f7
Asano1989 <asano1989.b@gmail.com>
2026-01-26 12:06:35 +0900

    change: 本番環境用の記述に変更

--- db/migrate/20260126020001_add_family_to_diaries.rb
+++ db/migrate/20260126020001_add_family_to_diaries.rb
@@ -1,19 +1,5 @@
 class AddFamilyToDiaries < ActiveRecord::Migration[7.1]
   def change
-    # null: true（NULLを許可）でカラムを追加
-    add_reference :diaries, :family, foreign_key: true
-  
-    # 既存の日記データに、投稿したユーザーの family_id を流し込む（データマイグレーション）
-    up_only do
-      Diary.find_each do |diary|
-        # ユーザーが家族に所属している場合のみ、その family_id をセット
-        if diary.user.family_id
-          diary.update_column(:family_id, diary.user.family_id)
-        end
-      end
-    end
-
-    # 最後に NULL を禁止する制約をつける
-    change_column_null :diaries, :family_id, false
+    add_reference :diaries, :family, null: false, foreign_key: true
   end
 end
