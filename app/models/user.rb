93834c805eb308e100060c3d1d7172434613175e
Asano1989 <asano1989.b@gmail.com>
2026-03-12 10:01:52 +0900

    delete: 不要なコメント、未使用のメソッドやバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   has_one :owned_family, class_name: 'Family', foreign_key: 'owner_id'
   has_one_attached :avatar
   has_many :diaries
-  # TODO: 将来的にユーザー情報削除機能を実装した場合に、以下の記述に変更する
-  # has_many :diaries, dependent: :nullify
   has_many :reactions
 
   attr_accessor :password, :password_confirmation
@@ -14,9 +12,6 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, on: :create
   validate :password_match, on: :create
   validates :name, presence: true
-
-  # 招待時のロジックなどで、既に家族がいる場合はエラーにする
-  validate :must_not_belong_to_multiple_families, on: :create_membership
 
   def can_create_family?
     family_id.nil? && owned_family.nil?
@@ -31,7 +26,7 @@ class User < ApplicationRecord
       # 外部ストレージのURLを返す
       Rails.application.routes.url_helpers.url_for(avatar)
     else
-      # assets内などのデフォルト画像のパスを返す
+      # デフォルト画像のパスを返す
       ActionController::Base.helpers.asset_path('default-avatar.png')
     end
   end
@@ -42,11 +37,5 @@ class User < ApplicationRecord
     return unless password != password_confirmation
 
     errors.add(:password_confirmation, "がパスワードと一致しません")
-  end
-
-  def must_not_belong_to_multiple_families
-    return unless family_id.present?
-
-    errors.add(:family, "は既に登録済みです。他の家族に参加することはできません。")
   end
 end
