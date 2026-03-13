d4f69c3d77661b58032368b2100f87e63d9933d0
Asano1989 <asano1989.b@gmail.com>
2026-03-13 15:17:17 +0900

    add: usersテーブルのemailに一意制約を追加

---
+++ db/migrate/20260313061211_add_unique_index_to_users_email.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToUsersEmail < ActiveRecord::Migration[7.1]
+  def change
+    add_index :users, 'LOWER(email)', unique: true, name: 'index_users_on_lower_email'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_26_020001) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_13_061211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -109,6 +109,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_26_020001) do
     t.datetime "updated_at", null: false
     t.string "supabase_uid"
     t.bigint "family_id"
+    t.index "lower((email)::text)", name: "index_users_on_lower_email", unique: true
     t.index ["family_id"], name: "index_users_on_family_id"
     t.index ["supabase_uid"], name: "index_users_on_supabase_uid", unique: true
   end
