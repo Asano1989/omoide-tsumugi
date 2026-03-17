c007334b04cd9fde3adbad0b3c808da0fe74a460
Asano1989 <asano1989.b@gmail.com>
2026-03-17 11:12:05 +0900

    update: associationを追記してFactoryの定義を更新

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -5,5 +5,24 @@ FactoryBot.define do
     association :user
     association :emoji
     association :family
+    # 1人の子どもを関連付ける
+    after(:create) do |diary|
+      create(:diary_child, diary: diary, child: create(:child, family: diary.family))
+    end
+
+    trait :with_two_children do
+      after(:create) do |diary|
+        diary.diary_children.destroy_all
+        2.times do
+          create(:diary_child, diary: diary, child: create(:child, family: diary.family))
+        end
+      end
+    end
+
+    trait :without_children do
+      after(:create) do |diary|
+        diary.diary_children.destroy_all
+      end
+    end
   end
 end
